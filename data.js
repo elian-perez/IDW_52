1c53b8682c590bf684fd0d7d03d7fd8bbc5f3407
NatyPellegrineschi <nataliapellegrineschi@gmail.com>
2025-10-20 16:25:20 -0300

    Agregado sistema de administración de médicos y botones de navegación

---
+++ data.js
@@ -0,0 +1,51 @@
+// data.js
+export const medicosIniciales = [
+  {
+    id: 1,
+    nombre: "Dr. Jesús María Amor",
+    especialidad: "Cardiología",
+    disponibilidad: "las 24 horas",
+    obraSocial: "Buscando la Cura",
+    foto: "img/cardiólogo.jpg"
+  },
+  {
+    id: 2,
+    nombre: "Dr. Mario Miguel Trozado",
+    especialidad: "Cirugía",
+    disponibilidad: "a veces",
+    obraSocial: "Ya Vuelvo",
+    foto: "img/cirujano.jpg"
+  },
+  {
+    id: 3,
+    nombre: "Dr. Atilio Simón Garabato",
+    especialidad: "Pediatría",
+    disponibilidad: "lunes a lunes por la tarde",
+    obraSocial: "La Liga de la justicia",
+    foto: "img/pediatra.jpg"
+  },
+  {
+    id: 4,
+    nombre: "Dra. Patricia Lorena Feliz",
+    especialidad: "Gastroenterología",
+    disponibilidad: "Sábados por la noche y domingos al mediodía",
+    obraSocial: "El Bodegón",
+    foto: "img/gastroenteróloga.jpg"
+  },
+  {
+    id: 5,
+    nombre: "Dra. Ernestina Luz Del Campo",
+    especialidad: "Oftalmología",
+    disponibilidad: "las 24 horas",
+    obraSocial: "Los Ojos que Hablan",
+    foto: "img/oftalmóloga.jpg"
+  },
+  {
+    id: 6,
+    nombre: "Dr. Benjamín Anselmo Mento",
+    especialidad: "Psicología",
+    disponibilidad: "Lunes a Viernes por la tarde",
+    obraSocial: "Relajate",
+    foto: "img/psicólogo.jpg"
+  }
+];
