1ffdd9ca6a55f29eb9f3553c00f8da0afbef2852
[elian-perez] <[elianperez95@gmail.com]>
2025-11-13 19:34:12 -0300

    Prioridad de carga a script  BASE64 y optimizado

--- js/base64-loader.js
+++ js/base64-loader.js
@@ -1,20 +1,19 @@
-document.addEventListener("DOMContentLoaded", async () => {
+// Carga rápido al llegar al SCRITPT
+(async () => {
   try {
     const res = await fetch("js/imagenes_base64.json");
     const data = await res.json();
 
-
     window.reemplazarImagenes = () => {
       document.querySelectorAll("img[data-src]").forEach(img => {
         let src = img.getAttribute("data-src")
-          ?.replace(/^\.?\/*/, "")   
-          ?.replace(/^img\//, "");   
+          ?.replace(/^\.?\/*/, "")
+          ?.replace(/^img\//, "");
 
         if (data[src]) {
           img.src = data[src];
           console.log(`Reemplazada desde JSON: ${src}`);
         } else {
-          
           if (!src.startsWith("data:image")) {
             img.src = img.getAttribute("data-src");
           }
@@ -28,7 +27,7 @@ document.addEventListener("DOMContentLoaded", async () => {
           ?.replace(/^img\//, "");
         if (data[src]) {
           favicon.href = data[src];
-          console.log(`Favicon reemplazado correctamente: ${src}`);
+          console.log(`Favicon reemplazado: ${src}`);
         }
       }
     };
@@ -38,4 +37,4 @@ document.addEventListener("DOMContentLoaded", async () => {
   } catch (err) {
     console.error("Error cargando imágenes Base64:", err);
   }
-});
+})();
